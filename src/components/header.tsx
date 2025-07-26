88837554e8b027b2a36482f287a287881b75bf6e
dflr10 <dafelor.10@hotmail.com>
2025-07-26 00:50:08 -0500

    feature(header): The position of the shopping cart and the project title are adjusted

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
           <div className="mr-4 flex">
             <a href="/" className="flex items-center space-x-2">
               <Brush className="h-6 w-6 text-primary" />
-              <span className="font-bold">Inocencia</span>
+              <span className="font-bold">Inocencia Studio</span>
             </a>
           </div>
           <div className="flex flex-1 items-center justify-end space-x-4">
