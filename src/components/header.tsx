00a99c7fcd4f5b5ab7c486390f0a76654481551a
Daniel Lozada <daniellozadarm@gmail.com>
2025-07-26 00:39:21 +0000

    hace poco ajustaste el header en dispositivos desktop para que tuviera u

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
   return (
     <>
       <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="container flex h-16 items-center px-4 sm:px-6 lg:px-20">
+        <div className="container flex h-16 items-center px-4 sm:px-6 lg:px-8">
           <div className="mr-4 flex">
             <a href="/" className="flex items-center space-x-2">
               <Brush className="h-6 w-6 text-primary" />
