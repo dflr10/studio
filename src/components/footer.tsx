dfcd9564216376533780bf93a6f822fee081a0bf
dflr10 <dafelor.10@hotmail.com>
2025-07-26 00:51:55 -0500

    feature(footer): The footer text was adjusted

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="w-full bg-secondary py-6">
       <div className="container mx-auto text-center text-secondary-foreground">
-        <p className="text-sm">Tu estilo, tu esencia.</p>
+        <p className="text-sm">Más que tu estilo, tu esencia.</p>
       </div>
     </footer>
   );
